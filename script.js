e6de53e74562a4eb2c7a0f2f3270f8be12a84d4e
Daniel R <110840284+Daniel-robe@users.noreply.github.com>
2023-02-13 20:38:19 -0600

    Fixed api calls and updated search history storage in local storage

--- script.js
+++ script.js
@@ -8,9 +8,38 @@ var apiKey = "c453978e4dc09fda7a9d8d6ba43fda70";
 //Page elements
 var searchForm = document.getElementById("search-form");
 var searchInput = document.getElementById("search-input");
-var searchHistory = document.getElementById("search-history");
+var searchHistoryEl = document.getElementById("search-history");
+var historyList = document.getElementById("history-list");
 var todaySection = document.getElementById("today");
 var forecastSection = document.getElementById("forecast");
+
+function getSearchHistory() {
+    if(localStorage.hasOwnProperty("searchHistory")){
+        searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+    }
+}
+
+function renderSearchHistory() {
+    getSearchHistory();
+    while(historyList.firstChild){
+        historyList.removeChild(historyList.firstChild);
+    }
+
+    for(var i = searchHistory.length-1; i>=0; i--){
+        var li = document.createElement("li");
+        li.textContent = searchHistory[i];
+        historyList.appendChild(li);
+    }
+}
+
+function addHistory() {
+    getSearchHistory();
+    if(!searchHistory.includes(currentCity)){
+        searchHistory.push(currentCity);
+    }
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+    renderSearchHistory();
+}
 
 function handleSubmit(event) {
     if(!searchInput.value){
@@ -19,9 +48,8 @@ function handleSubmit(event) {
 
     event.preventDefault();
     currentCity = searchInput.value;
-    console.log(currentCity);
+    addHistory();
     getCityCoords();
-    getWeather();
 }
 
 function getCityCoords() {
@@ -30,11 +58,12 @@ function getCityCoords() {
     fetch(apiUrl).then(function(res) {
         return res.json();
     }).then(function(data) {
-        if(data[0]) {
+        if(!data[0]) {
             alert("City not found");
         } else {
             cityLat = data[0].lat;
             cityLon = data[0].lon;
+            getWeather();
         }
     }).catch(function (err) {
         console.error(err);
@@ -42,8 +71,20 @@ function getCityCoords() {
 }
 
 function getWeather() {
-    var apiUrl = `http://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&units=imperial&exclude=minutely,hourly&appid=${apiKey}`
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${cityLat}&lon=${cityLon}&appid=${apiKey}&units=imperial`;
 
+    fetch(apiUrl).then(function(res) {
+        return res.json();
+    }).then(function(data) {
+        console.log(data);
+        getForecast();
+    }).catch(function (err) {
+        console.error(err);
+    });
+}
+
+function getForecast() {
+    var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&appid=${apiKey}&units=imperial`;
     fetch(apiUrl).then(function(res) {
         return res.json();
     }).then(function(data) {
@@ -54,3 +95,4 @@ function getWeather() {
 }
 
 searchForm.addEventListener('submit', handleSubmit);
+renderSearchHistory();
